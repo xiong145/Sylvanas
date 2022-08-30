5ed85e18a6dce13966658811a2e70eedac62d80c
xiong <xionghou@ztoky.cn>
2022-08-30 11:19:45 +0800

    08

--- sylvanas-user/sylvanas-user-service/src/main/java/com/sylvanas/user/SylvanasUserApplication.java
+++ sylvanas-user/sylvanas-user-service/src/main/java/com/sylvanas/user/SylvanasUserApplication.java
@@ -2,8 +2,10 @@ package com.sylvanas.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SylvanasUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(SylvanasUserApplication.class, args);
