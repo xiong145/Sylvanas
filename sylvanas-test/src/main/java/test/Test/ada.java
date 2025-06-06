5edbe776d3fba1ae6556f03e7f4cf6d1f68fae17
xiong <xionghou145@gmail.com>
2025-06-07 01:22:28 +0800

    18

--- sylvanas-test/src/main/java/test/Test/ada.java
+++ sylvanas-test/src/main/java/test/Test/ada.java
@@ -1,19 +1,19 @@
-package test.Test;
-
-import com.baomidou.dynamic.datasource.annotation.DSTransactional;
-
-@DSTransactional
-public class ada {
-    public static void main(String[] args) {
-        Double a = 0.0;
-        if (a.equals(0.0)) {
-            System.out.println(1);
-        }
-        if (a.equals(0.00)) {
-            System.out.println(2);
-        }
-        if(a.equals(0)){
-            System.out.println(3);
-        }
-    }
-}
+//package test.Test;
+//
+//import com.baomidou.dynamic.datasource.annotation.DSTransactional;
+//
+//@DSTransactional
+//public class ada {
+//    public static void main(String[] args) {
+//        Double a = 0.0;
+//        if (a.equals(0.0)) {
+//            System.out.println(1);
+//        }
+//        if (a.equals(0.00)) {
+//            System.out.println(2);
+//        }
+//        if(a.equals(0)){
+//            System.out.println(3);
+//        }
+//    }
+//}
--- sylvanas-test/src/main/java/test/server/GlobalKeyListenerExample.java
+++ sylvanas-test/src/main/java/test/server/GlobalKeyListenerExample.java
@@ -1,47 +1,47 @@
-package test.server;
-
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
-import org.jnativehook.keyboard.NativeKeyEvent;
-import org.jnativehook.keyboard.NativeKeyListener;
-
-public class GlobalKeyListenerExample implements NativeKeyListener {
-
-    @Override
-    public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
-        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
-            try {
-                GlobalScreen.unregisterNativeHook();
-            } catch (NativeHookException nativeHookException) {
-                nativeHookException.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void nativeKeyReleased(NativeKeyEvent e) {
-        GlobalKeyListenerFile globalKeyListenerFile = new GlobalKeyListenerFile();
-        globalKeyListenerFile.fileInput(NativeKeyEvent.getKeyText(e.getKeyCode()));
-        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-    }
-
-    @Override
-    public void nativeKeyTyped(NativeKeyEvent e) {
-        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
-    }
-
-    public static void main(String[] args) {
-        try {
-            GlobalScreen.registerNativeHook();
-        } catch (NativeHookException ex) {
-            System.err.println("There was a problem registering the native hook.");
-            System.err.println(ex.getMessage());
-
-            System.exit(1);
-        }
-
-        GlobalScreen.addNativeKeyListener(new GlobalKeyListenerExample());
-    }
-}
+//package test.server;
+//
+//import org.jnativehook.GlobalScreen;
+//import org.jnativehook.NativeHookException;
+//import org.jnativehook.keyboard.NativeKeyEvent;
+//import org.jnativehook.keyboard.NativeKeyListener;
+//
+//public class GlobalKeyListenerExample implements NativeKeyListener {
+//
+//    @Override
+//    public void nativeKeyPressed(NativeKeyEvent e) {
+//        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//
+//        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+//            try {
+//                GlobalScreen.unregisterNativeHook();
+//            } catch (NativeHookException nativeHookException) {
+//                nativeHookException.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void nativeKeyReleased(NativeKeyEvent e) {
+//        GlobalKeyListenerFile globalKeyListenerFile = new GlobalKeyListenerFile();
+//        globalKeyListenerFile.fileInput(NativeKeyEvent.getKeyText(e.getKeyCode()));
+//        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//    }
+//
+//    @Override
+//    public void nativeKeyTyped(NativeKeyEvent e) {
+//        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+//    }
+//
+//    public static void main(String[] args) {
+//        try {
+//            GlobalScreen.registerNativeHook();
+//        } catch (NativeHookException ex) {
+//            System.err.println("There was a problem registering the native hook.");
+//            System.err.println(ex.getMessage());
+//
+//            System.exit(1);
+//        }
+//
+//        GlobalScreen.addNativeKeyListener(new GlobalKeyListenerExample());
+//    }
+//}
